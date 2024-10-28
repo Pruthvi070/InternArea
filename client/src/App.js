25e17797470cc0a5cba9b1cca5ced9748824031d
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-10-28 11:56:25 +0530

    work on footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import Navbar from './Components/Navbar/Navbar.jsx'
 import Sidebar from './Components/Navbar/Sidebar.jsx';
 
 import Home from './Components/Home/Home.jsx';
+import Footer from './Components/Footerr/Footer.jsx';
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home/>
+      <Footer/>
 
     </div>
   )
