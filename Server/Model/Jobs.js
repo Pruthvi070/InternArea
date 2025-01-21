19e8fc8c0bd0465146e25fa6c053213da52af84f
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2025-01-21 16:09:16 +0530

    api

--- Server/Model/Jobs.js
+++ Server/Model/Jobs.js
@@ -1,28 +1,29 @@
-const mongoose = require("mongoose")
-const JobShcema = new mongoose.Schema({
+const mongoose=require("mongoose")
+ const JobShcema=new mongoose.Schema({
     title: String,
     company: String,
     location: String,
     Experience: String,
     category: String,
-    aboutCompany: String,
-    aboutJob: String,
+    aboutCompany:String,
+    aboutJob:String,
     Whocanapply: String,
     perks: Array,
-    AdditionalInfo: String,
+    AdditionalInfo:String,
     CTC: String,
-    StartDate: String,
-
-    createAt: {
-        type: Date,
-        default: Date.now,
+    StartDate:String,
+    
+    createAt:{
+        type:Date,
+        default:Date.now,
     },
 
+   
+   
+ })
+ module.exports=mongoose.model("Job",JobShcema)
 
 
-})
-module.exports = mongoose.model("Job", JobShcema)
 
-
-
-
+ 
+    
