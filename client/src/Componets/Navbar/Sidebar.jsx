56adc35c95a2774161c6cc0e81662aa83acbcbeb
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2025-01-22 16:52:02 +0530

    final

--- client/src/Componets/Navbar/Sidebar.jsx
+++ client/src/Componets/Navbar/Sidebar.jsx
@@ -8,8 +8,8 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase';
 
 function Sidebar() {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-const navigate=useNavigate()
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const navigate = useNavigate()
   const openSidebar = () => {
     setSidebarOpen(true);
   };
@@ -31,136 +31,136 @@ const navigate=useNavigate()
       document.removeEventListener('click', handleOutsideClick);
     };
   }, [sidebarOpen]);
-  const logoutFunction=()=>{
+  const logoutFunction = () => {
     signOut(auth)
     navigate("/")
-  
-}
-  const user=useSelector(selectUser)
+
+  }
+  const user = useSelector(selectUser)
   return (
 
     <>
-  <div className="App2 -mt-2 overflow-hidden"  >
-      <Link to="/">
-  <img src={logo} alt=""  id='nav2-img'/>    </Link>  
+      <div className="App2 -mt-2 overflow-hidden"  >
+        <Link to="/">
+          <img src={logo} alt="" id='nav2-img' />    </Link>
         <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-          <span  className="cursor-pointer close-btn" onClick={closeSidebar}>
+          <span className="cursor-pointer close-btn" onClick={closeSidebar}>
             &times;
           </span>
-          {user?(
-  <>
-  <div className="profile">
-   <Link to={"/profile"}>
-   <img className='rounded-full justify-center' src={user.photo} alt="" srcset="" />
-   </Link> 
-    <p className=' text-center'>Profile name <span className='font-bold text-blue-500'>{user?.name}</span></p>
-  </div>
-  </>
-):
-(
+          {user ? (
+            <>
+              <div className="profile">
+                <Link to={"/profile"}>
+                  <img className='rounded-full justify-center' src={user.photo} alt="" srcset="" />
+                </Link>
+                <p className=' text-center'>Profile name <span className='font-bold text-blue-500'>{user?.name}</span></p>
+              </div>
+            </>
+          ) :
+            (
 
-  
-  <div className="auth">
-  
-</div>
-  ) 
-}
+
+              <div className="auth">
+
+              </div>
+            )
+          }
           <Link to="/internship">internships </Link>
-    <Link to="/Jobs">Jobs  </Link>
-       
-       <Link to={"/"} className='small'>contact Us</Link> 
-<hr />
-{user?(
-  <>
-  <div className="addmore">
-    
-    {user?(
-  <Link to={"/userapplication"}>
-  <p>My Applications</p>
-  </Link>
-    ):(
-      <Link to={"/register"}>
-      <p>My Applications</p>
-      </Link>
-    )
+          <Link to="/Jobs">Jobs  </Link>
 
-    }
+          <Link to={"/"} className='small'>contact Us</Link>
+          <hr />
+          {user ? (
+            <>
+              <div className="addmore">
 
-  <Link>
-  
-  <p>View Resume</p>
-  </Link>
-  <Link>
-  <p>More</p>
-  </Link>
-  <button className='bt-log' id='bt' onClick={logoutFunction}>Logout <i class="bi bi-box-arrow-right"></i></button>
-  <br />
-  <br />
-<button onClick={logoutFunction}>Log Out <i class="bi bi-box-arrow-right"></i></button>
-  
-  </div>
-  </>
-):
-(
+                {user ? (
+                  <Link to={"/userapplication"}>
+                    <p>My Applications</p>
+                  </Link>
+                ) : (
+                  <Link to={"/register"}>
+                    <p>My Applications</p>
+                  </Link>
+                )
 
-  
-  <div className="addmore">
-  <p>Register- As a Student</p>
-  <p>Register- As a Employer</p>
-  <br />
-  <br />
+                }
 
-  </div>
-  ) 
-}
+                <Link>
+
+                  <p>View Resume</p>
+                </Link>
+                <Link>
+                  <p>More</p>
+                </Link>
+                <button className='bt-log' id='bt' onClick={logoutFunction}>Logout <i class="bi bi-box-arrow-right"></i></button>
+                <br />
+                <br />
+                <button onClick={logoutFunction}>Log Out <i class="bi bi-box-arrow-right"></i></button>
+
+              </div>
+            </>
+          ) :
+            (
+
+
+              <div className="addmore">
+                <p>Register- As a Student</p>
+                <p>Register- As a Employer</p>
+                <br />
+                <br />
+
+              </div>
+            )
+          }
 
         </div>
 
 
         <div className="main">
-          <span style={{fontSize:"22px"}} className="open-btn" onClick={openSidebar}>
-            &#9776; 
+          <span style={{ fontSize: "22px" }} className="open-btn" onClick={openSidebar}>
+            &#9776;
           </span>
         </div>
-        
-<div className="search2">
-<i class="bi bi-search"></i>
-    <input type="search"  placeholder='Search'/>
-</div>
+
+        <div className="search2">
+          <i class="bi bi-search"></i>
+          <input type="search" placeholder='Search' />
+        </div>
 
 
-{user?(
-  <>
+        {user ? (
+          <>
 
-  </>
-):
-(
+          </>
+        ) :
+          (
 
-  <>
-  <div className="reg">
-    
-  <Link to="/register" >   <button  className='btn4'>
-  Register</button></Link>
-  </div>
-  <div className="admin">
+            <>
+              <div className="reg">
 
-<Link to={"/adminLog"}>
-<button id='admin'> Admin Login</button>
-</Link>
-</div>
-  </>
+                <Link to="/register" >   <button className='btn4'>
+                  Register</button></Link>
+              </div>
+              <div className="admin">
 
-
-
-  ) 
-}
+                <Link to={"/adminLog"}>
+                  <button id='admin'> Admin Login</button>
+                </Link>
+              </div>
+            </>
 
 
-<p className='text-red-300'>Hire Talent</p>
+
+          )
+        }
+
+
+        <p className='text-red-300'>Hire Talent</p>
 
       </div>
     </>
-    
+
   )
 }
 
